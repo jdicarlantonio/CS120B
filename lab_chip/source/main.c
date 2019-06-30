78ca03d713c8be399d1ee65091ef04fee04961e7
jd <jd@0x29A>
2019-06-29 20:53:01 -0700

    lab5 again

--- lab_chip/source/main.c
+++ lab_chip/source/main.c
@@ -15,11 +15,25 @@
 
 int main(void) 
 {
+    DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
 
+    unsigned char led = 0x00;
+    unsigned char button = 0x00;
     while (1) 
     {
-        PORTB = 0x0F;
+        button = ~PINA & 0x01;
+
+        if(button)
+        {
+            led = (led & 0xFC) | 0x01;
+        }
+        else
+        {
+            led = (led & 0xFC) | 0x02;
+        }
+
+        PORTB = led;
     }
     return 1;
 }
